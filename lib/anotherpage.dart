de312273ad0160baf818b9ac8fc960602132b00a
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2024-06-26 14:52:48 +0600

    push dilam

---
+++ lib/anotherpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnotherPage extends StatelessWidget {
+  const AnotherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Another Page"),
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gdrivetest/anotherpage.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   Future<void> openAppWebView(String url) async {
     if (!await launchUrl(Uri.parse(url), mode: LaunchMode.inAppWebView)) {
       throw Exception('Could not launch $url');
@@ -16,6 +23,7 @@ class HomePage extends StatelessWidget {
     }
   }
 
+
   Future<void> openExternalApplication(String url) async {
     if (!await launchUrl(Uri.parse(url),
         mode: LaunchMode.externalApplication)) {
@@ -23,6 +31,28 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    disableScreenshots();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    enableScreenshots();
+    super.dispose();
+  }
+
+
+  Future<void> disableScreenshots() async {
+    await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+  }
+
+  Future<void> enableScreenshots() async {
+    await FlutterWindowManager.clearFlags(FlutterWindowManager.FLAG_SECURE);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +86,15 @@ class HomePage extends StatelessWidget {
                 onPressed: () {
                   openExternalApplication(
                       'https://pub.dev/packages/url_launcher');
+                },
+                child: const Text(
+                  'External Application',
+                  style: TextStyle(color: Colors.white),
+                )),
+            ElevatedButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>AnotherPage()));
                 },
                 child: const Text(
                   'External Application',
